34c030d1b2b54f30dcf6bf7ea515431022ac4686
dhdaviescode-star <dhdavies.code@gmail.com>
2026-07-07 10:21:12 +0100

    Finish responsive layout and CSS architecture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,20 @@
-// Back to Top Button Functionality
+//======================================
+// BACK TO TOP BUTTON
+//======================================
 
-// Get the button element
-const backToTopBtn = document.getElementById('backToTopBtn');
+const backToTopBtn = document.getElementById("backToTopBtn");
 
-// Show the button when the user scrolls down 200px from the top of the document
-
-window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    backToTopBtn.style.display = 'block';
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopBtn.style.display = "block";
   } else {
-    backToTopBtn.style.display = 'none';
+    backToTopBtn.style.display = "none";
   }
-};
+});
 
-// Scroll to the top of the document when the button is clicked
-backToTopBtn.addEventListener('click', function() {
+backToTopBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
-}
-
+});
